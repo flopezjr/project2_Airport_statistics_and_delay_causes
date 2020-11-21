9f86dbb7c716574e02b6f0df26adb6fb4a987fb6
flopezjr <68476675+flopezjr@users.noreply.github.com>
2020-11-21 16:26:11 -0600

    update schemata

--- db/schema.sql
+++ db/schema.sql
@@ -1,13 +1,13 @@
 --
 Add flight table
 -- create flights_metadata table
-CREATE TABLE flights_metadata (
-    uid varchar(6) PRIMARY KEY,
-    flight_date date,
-    flight_year year,
+CREATE TABLE airport_delays (
+    uid varchar(6),
+    flight_year date,
+    flight_month date,
     carrier varchar(5),
     carrier_name varchar(45),
-    locationID varchar(5),
+    locationID varchar(5) PRIMARY KEY,
     airport_name varchar(60),
     arr_flights int,
     arr_del15 int,
@@ -17,34 +17,7 @@ CREATE TABLE flights_metadata (
     security_ct int,
     late_aircraft_ct int,
     arr_cancelled int,
-    arr_diverted int
-    arr_delay int,
-    carrier_delay int,
-    weather_delay int,
-    nas_delay int,
-    security_delay int,
-    late_aircraft_delay int
-);
-
-  
--- create topairports table
-CREATE TABLE topairports_metadata (
-    blank varchar(6) PRIMARY KEY,
-    flight_date date,
-    flight_year year,
-    carrier varchar(5),
-    carrier_name varchar(45),
-    airport varchar(5),
-    airport_name varchar(60),
-    arr_flights int,
-    arr_del15 int,
-    carrier_ct int,
-    weather_ct int,
-    nas_ct int,
-    security_ct int,
-    late_aircraft_ct int,
-    arr_cancelled int,
-    arr_diverted int
+    arr_diverted int,
     arr_delay int,
     carrier_delay int,
     weather_delay int,
@@ -55,9 +28,9 @@ CREATE TABLE topairports_metadata (
 
 -- create airport_codes table
 CREATE TABLE airport_codes (
-    locationID varchar(5) references topairports_metadata(airport),
-    Latitude float(7,4),
-    Longitude float(6,4)
+    locationID varchar(5) references airport_delays(locationID),
+    Latitude decimal(10,8),
+    Longitude decimal(11, 8)
 );
 
 
