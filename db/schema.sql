207ded2ec21e065c48710c9d0944aebdf3934bc7
Felipe Lopez <you@example.commr.felipelopez@gmail.com>
2020-11-25 00:27:23 +0000

    redid schemata

--- db/schema.sql
+++ db/schema.sql
@@ -1,33 +1,38 @@
 --
 Add flight table
 -- create flights_metadata table
+
 CREATE TABLE airport_delays (
-    uid varchar(6),
-    flight_year date,
-    flight_month date,
+    uid varchar(6) PRIMARY KEY,
+    flight_year int,
+    flight_month int,
     carrier varchar(5),
     carrier_name varchar(45),
-    locationID varchar(5) PRIMARY KEY,
-    airport_name varchar(60),
+    locationID varchar(5),
+    airport_name varchar(120),
     arr_flights int,
     arr_del15 int,
-    carrier_ct int,
-    weather_ct int,
-    nas_ct int,
-    security_ct int,
-    late_aircraft_ct int,
+    carrier_ct float,
+    security_ct float,
+    late_aircraft_ct float,
     arr_cancelled int,
     arr_diverted int,
-    arr_delay int,
-    carrier_delay int,
     weather_delay int,
     nas_delay int,
     security_delay int,
     late_aircraft_delay int
 );
 
--- create airport_codes table
 CREATE TABLE airport_codes (
+    continent varchar(10),
+    locationID varchar(5) references airport_delays(locationID),
+    Latitude DECIMAL(10,5),
+    Longitude DECIMAL(11, 5)
+);
+
+-- create airport_codes table - old version
+CREATE TABLE airport_codes (
+    continent varchar(5),
     locationID varchar(5) references airport_delays(locationID),
     Latitude decimal(10,8),
     Longitude decimal(11, 8)
@@ -59,7 +64,30 @@ CREATE TABLE topairports_metadata (
     late_aircraft_delay int
 );
 
-
+CREATE TABLE airport_delays (
+    uid varchar(6),
+    flight_year integer,
+    flight_month integer,
+    carrier varchar(5),
+    carrier_name varchar(45),
+    locationID varchar(5) PRIMARY KEY,
+    airport_name varchar(60),
+    arr_flights int,
+    arr_del15 int,
+    carrier_ct float,
+    weather_ct int,
+    nas_ct float,
+    security_ct float,
+    late_aircraft_ct float,
+    arr_cancelled int,
+    arr_diverted int,
+    arr_delay int,
+    carrier_delay int,
+    weather_delay int,
+    nas_delay int,
+    security_delay int,
+    late_aircraft_delay int
+);
 
 -- create airport_codes table
 CREATE TABLE airport_codes (
